59b30c7ca9847df5b2c7524b695f4eb6a1770ad6
ravjot <ravjotkaamra@gmail.com>
2021-04-20 15:11:44 +0530

    added axios for get req

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
+import axios from "axios";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", number: "040-123456" },
-    { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-    { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-    { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilter] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const filteredPersons = persons.filter((person) =>
     person.name.toUpperCase().includes(filterName.slice().trim().toUpperCase())
@@ -54,7 +60,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter text="filter name with " filterName={filterName} handleFiltering={handleFiltering}/>
+      <Filter
+        text="filter name with "
+        filterName={filterName}
+        handleFiltering={handleFiltering}
+      />
 
       <h3>add a new</h3>
       <PersonForm
