283111dfe91b119d8e6907e471db09244e329a6d
ravjot <ravjotkaamra@gmail.com>
2021-04-26 23:58:33 +0530

    add: morgan for logging

--- part_3/index.js
+++ part_3/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+morgan.token("post", (req) =>
+  req.method === "POST" ? JSON.stringify(req.body) : " "
+);
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :post")
+);
 
 let persons = [
   {
@@ -77,7 +87,7 @@ const generateId = () => {
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-  console.log("body :>> ", body);
+
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: "name or number is missing",
