0ce1ff256ba6aacd35b0f42f0915a5c853f351c9
ravjot <ravjotkaamra@gmail.com>
2021-04-30 23:20:08 +0530

    added: heroku and build directory

--- part_3/index.js
+++ part_3/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('build'))
 
 morgan.token("post", (req) =>
   req.method === "POST" ? JSON.stringify(req.body) : " "
@@ -110,7 +113,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
