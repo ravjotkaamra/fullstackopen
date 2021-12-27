d0ad795496fe4d54ffb6355f325196c391779a39
ravjot <ravjotkaamra@gmail.com>
2021-12-27 19:30:39 +0530

    completed: exercise 8.3

--- part_8/gql-library/index.js
+++ part_8/gql-library/index.js
@@ -107,7 +107,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]
+    allBooks(author: String): [Book!]
     allAuthors: [Author!]
   }
 `;
@@ -118,7 +118,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+
+      return books.filter((book) => book.author === args.author);
+    },
     allAuthors: () => authors,
   },
 
