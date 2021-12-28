f42475475e46924685d8a036ff2c52e8d6ce3e92
ravjot <ravjotkaamra@gmail.com>
2021-12-28 16:25:56 +0530

    exercise 8.10 completed

---
+++ part_8/gql-library-frontend/src/components/Books.js
@@ -0,0 +1,41 @@
+import { useQuery } from "@apollo/client";
+import React from "react";
+import { ALL_BOOKS } from "../queries";
+
+const Books = (props) => {
+  const { loading, error, data } = useQuery(ALL_BOOKS);
+
+  if (!props.show) {
+    return null;
+  }
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :{error.message} </p>;
+
+  const books = data.allBooks;
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {books.map((b) => (
+            <tr key={b.id}>
+              <td>{b.title}</td>
+              <td>{b.author}</td>
+              <td>{b.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Books;
---
+++ part_8/gql-library-frontend/src/queries.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`;
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      id
+      author
+      published
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation createBook(
+    $title: String!
+    $published: Int!
+    $author: String!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+      id
+    }
+  }
+`;
